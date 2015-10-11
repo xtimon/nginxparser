a8eadf6439ad54c1fe198846a160021130a38cbb
Тимур Исанов <timur.isanov@bars-open.ru>
2015-10-11 19:42:23 +0300

    Time and number reports

--- nginx_parser.py
+++ nginx_parser.py
@@ -20,10 +20,13 @@ def progress_bar(progress):
     stdout.flush()
 
 
-def analyze_log(logfile, outfile, time):
+def analyze_log(logfile, outfile, time, count, exclude):
     summary = {'by_types': {'Overall': 0},
                'by_time': {'Overall': 0},
                'by_status': {}}
+    if time or count:
+        time_total = {}
+        count_total = {}
     log_line_nu = 0
     lines_count = sum(1 for l in open(logfile))
     progress = 0
@@ -50,6 +53,9 @@ def analyze_log(logfile, outfile, time):
             args = line_opts[0][9]
             request_time = float(line_opts[0][10])
 
+            if exclude and exclude in request:
+                continue
+
 # Creation the summary
 # By types
             summary['by_types']['Overall'] += 1
@@ -71,6 +77,17 @@ def analyze_log(logfile, outfile, time):
             else:
                 summary['by_status'][status] = 1
 
+# Creation the total timing and the count report
+            if time or count:
+                if request in time_total.keys():
+                    time_total[request] += request_time
+                else:
+                    time_total[request] = request_time
+                if request in count_total.keys():
+                    count_total[request] += 1
+                else:
+                    count_total[request] = 1
+
 # Sort the summary dicts
     sorted_summary_by_types = sorted(summary['by_types'].items(), key=itemgetter(1), reverse=True)
     sorted_summary_by_time = sorted(summary['by_time'].items(), key=itemgetter(1), reverse=True)
@@ -91,19 +108,58 @@ def analyze_log(logfile, outfile, time):
         summary_by_status += "{}: {} ".format(k[0], k[1])
     print(summary_by_status)
 
+# Sort the total timing dict
+    if time:
+        sorted_time_total = sorted(time_total.items(), key=itemgetter(1), reverse=True)
+
+# Sort the total count dict
+    if count:
+        sorted_count_total = sorted(count_total.items(), key=itemgetter(1), reverse=True)
+
+# Print the total timing report
+    if time:
+        print("\n= The report, based on the total call time {}".format("=" * 64))
+        print("|{0:>17}|{1:>20}|{2:>17}| {3:<}".\
+              format("Calls", "Total time (sec)", "Resp. rate (s/c)", "URL pattern"))
+        for e in sorted_time_total:
+            print("|{0:>17}|{1:>20}|{2:>17}| {3:<}".\
+                  format(count_total[e[0]], round(e[1], 2), round(e[1] / count_total[e[0]], 2), e[0]))
+
+# Print the count report
+    if count:
+        print("\n= The report, based on the total number of queries {}".format("=" * 56))
+        print("|{0:>17}|{1:>20}|{2:>17}| {3:<}".\
+              format("Calls", "Total time (sec)", "Resp. rate (s/c)", "URL pattern"))
+        for e in sorted_count_total:
+            print("|{0:>17}|{1:>20}|{2:>17}| {3:<}".\
+                  format(e[1], round(time_total[e[0]], 2), round(time_total[e[0]] / e[1], 2), e[0]))
+
 
 def usage():
-    print("Usage info")
+    print('''
+Usage: ./nginx_parser.py [OPTION]... -l logfile.log
+Mandatory arguments to long options are mandatory for short options too.
+  -l, --logfile                 Log file for analysis
+  -o, --outfile                 File to output reports (in developing)
+  -t, --time                    Print the report, based on the total call time
+  -c, --count                   Print the report, based on the total number of queries
+  -e, --exclude                 The part of URL that are excluded from reporting
+EXAMPLES:
+  ./nginx_parser.py --count --logfile=/path/to/logfile.log
+  ./nginx_parser.py -ct -e /static/ -l /path/to/logfile.log
+    ''')
 
 
 def main():
     try:
-        opts, args = getopt.getopt(argv[1:], "htl:o:", ["help", "time", "logfile=", "outfile="])
+        opts, args = getopt.getopt(argv[1:], "htcl:o:e:", ["help", "time", "count", "logfile=", "outfile=", "exclude="])
     except getopt.GetoptError as err:
         print(err)
         usage()
         exit(2)
     time = False
+    count = False
+    exclude = ''
     logfile = None
     outfile = None
     for o, a in opts:
@@ -116,11 +172,15 @@ def main():
             outfile = a
         elif o in ("-t", "--time"):
             time = True
+        elif o in ("-c", "--count"):
+            count = True
+        elif o in ("-e", "--exclude"):
+            exclude = a
         else:
             assert False, "unhandled option"
     if logfile:
         if path.isfile(logfile):
-            analyze_log(logfile, outfile, time)
+            analyze_log(logfile, outfile, time, count, exclude)
         else:
             print("It is not a file: {}".format(logfile))
             usage()
