331edee5d0a45f0fafa9e348d0cf021434a76e40
Тимур Исанов <timur.isanov@bars-open.ru>
2015-10-11 15:58:15 +0300

    pep8

--- nginx_parser.py
+++ nginx_parser.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-import getopt, operator, os.path, re, sys
+import getopt
+from operator import itemgetter
+from os import path
+from re import compile
+from sys import argv, exit, stdout
 
 # Using log format:
 # log_format myformat '$remote_addr - [$time_local] "$host" "$request" '
@@ -8,11 +12,13 @@ import getopt, operator, os.path, re, sys
 
 # Creation of a regular expression for the format used
 log_format = '([\d.]+) \- \[(.+)\] "([\d.]+)" "(.+) (.+) .+" (\d{3}) \((\d+)\) "(.+)" "(.+) (.+)" \[([\d.]+)]'
-line_re = re.compile(log_format)
+line_re = compile(log_format)
+
 
 def progress_bar(progress):
-    sys.stdout.write('\r[{}{}] {}%'.format('#'*(progress), ' ' * (100-progress), progress))
-    sys.stdout.flush()
+    stdout.write('\r[{}{}] {}%'.format('#' * progress, ' ' * (100-progress), progress))
+    stdout.flush()
+
 
 def analyze_log(logfile, outfile, time):
     summary = {'by_types': {'Overall': 0},
@@ -47,20 +53,23 @@ def analyze_log(logfile, outfile, time):
             summary['by_types']['Overall'] += 1
             if request_type in summary['by_types'].keys():
                 summary['by_types'][request_type] += 1
-            else: summary['by_types'][request_type] = 1
+            else:
+                summary['by_types'][request_type] = 1
 # By time
             summary['by_time']['Overall'] += request_time
             if request_type in summary['by_time'].keys():
                 summary['by_time'][request_type] += request_time
-            else: summary['by_time'][request_type] = request_time
+            else:
+                summary['by_time'][request_type] = request_time
 # By status
             if status in summary['by_status'].keys():
                 summary['by_status'][status] += 1
-            else: summary['by_status'][status] = 1
+            else:
+                summary['by_status'][status] = 1
 # Sort the summary dicts
-    sorted_summary_by_types = sorted(summary['by_types'].items(), key=operator.itemgetter(1), reverse=True)
-    sorted_summary_by_time = sorted(summary['by_time'].items(), key=operator.itemgetter(1), reverse=True)
-    sorted_summary_by_status = sorted(summary['by_status'].items(), key=operator.itemgetter(1), reverse=True)
+    sorted_summary_by_types = sorted(summary['by_types'].items(), key=itemgetter(1), reverse=True)
+    sorted_summary_by_time = sorted(summary['by_time'].items(), key=itemgetter(1), reverse=True)
+    sorted_summary_by_status = sorted(summary['by_status'].items(), key=itemgetter(1), reverse=True)
 # Print the summary
     print("\n= Summary {}".format("=" * 97))
     summary_by_types = '| Request types\t\t: '
@@ -69,7 +78,7 @@ def analyze_log(logfile, outfile, time):
     print(summary_by_types)
     summary_by_time = '| Request timing\t: '
     for k in sorted_summary_by_time:
-        summary_by_time += "{}: {} ".format(k[0], round(k[1],2))
+        summary_by_time += "{}: {} ".format(k[0], round(k[1], 2))
     print(summary_by_time)
     summary_by_status = '| Request statuses\t: '
     for k in sorted_summary_by_status:
@@ -80,20 +89,21 @@ def analyze_log(logfile, outfile, time):
 def usage():
     print("Usage info")
 
+
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "htl:o:", ["help", "time", "logfile=", "outfile="])
+        opts, args = getopt.getopt(argv[1:], "htl:o:", ["help", "time", "logfile=", "outfile="])
     except getopt.GetoptError as err:
         print(err)
         usage()
-        sys.exit(2)
+        exit(2)
     time = False
     logfile = None
     outfile = None
     for o, a in opts:
         if o in ("-h", "--help"):
             usage()
-            sys.exit()
+            exit()
         elif o in ("-l", "--logfile"):
             logfile = a
         elif o in ("-o", "--output"):
@@ -103,13 +113,14 @@ def main():
         else:
             assert False, "unhandled option"
     if logfile:
-        if os.path.isfile(logfile):
+        if path.isfile(logfile):
             analyze_log(logfile, outfile, time)
         else:
             print("It is not a file: {}".format(logfile))
             usage()
     else:
         usage()
+
 
 if __name__ == "__main__":
     main()
