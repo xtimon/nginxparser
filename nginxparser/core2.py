1a3f8ecc6fc3e817a49a7cc7710d03f91a805695
xtimon <xtimon@yahoo.com>
2016-09-20 13:48:11 +0300

    count clients

--- nginxparser/core2.py
+++ nginxparser/core2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import re
-import timeit
 import json
 from datetime import datetime
 from patterns import log_format
@@ -66,6 +65,36 @@ def count_timeline():
         yield timeline
 
 
+def count_clients():
+    clients = {}
+    while True:
+        d = (yield)
+        if d:
+            remote_addr = d['remote_addr']
+            request_time = float(d['request_time'])
+            try:
+                upstream_time = float(d['upstream_time'])
+                responded = 1
+                responded_request_time = request_time
+            except ValueError:
+                upstream_time = 0
+                responded = 0
+                responded_request_time = 0
+            clients[remote_addr] = clients.get(remote_addr, {
+                "total_count": 0,
+                "responded": 0,
+                "total_request_time": 0,
+                "responded_request_time": 0,
+                "upstream_time": 0
+            })
+            clients[remote_addr]["total_count"] += 1
+            clients[remote_addr]["responded"] += responded
+            clients[remote_addr]["total_request_time"] += request_time
+            clients[remote_addr]["responded_request_time"] += responded_request_time
+            clients[remote_addr]["upstream_time"] += upstream_time
+        yield clients
+
+
 def parse(log_format=log_format, **kwargs):
     log_file = kwargs.get('log_file', '')
     out_json_file = kwargs.get('out_json_file', '')
@@ -74,8 +103,10 @@ def parse(log_format=log_format, **kwargs):
     parsed_count = 0
     cm = count_methods()
     ct = count_timeline()
+    cc = count_clients()
     next(cm)
     next(ct)
+    next(cc)
     for line in lines:
         m = re.match(log_format, line)
         total_count += 1
@@ -84,12 +115,15 @@ def parse(log_format=log_format, **kwargs):
             parsed_count += 1
             cm.send(parsed_line)
             ct.send(parsed_line)
+            cc.send(parsed_line)
         except AttributeError:
             print('Line {}: {} not parsed'.format(total_count, line))
     methods = cm.send(None)
     timeline = ct.send(None)
+    clients = cc.send(None)
     cm.close()
     ct.close()
+    cc.close()
     if out_json_file:
         with open(out_json_file, 'w') as ojf:
             json_timeline = {}
@@ -99,7 +133,8 @@ def parse(log_format=log_format, **kwargs):
                 "total_count": total_count,
                 "parsed_count": parsed_count,
                 "methods": methods,
-                "timeline": json_timeline
+                "timeline": json_timeline,
+                "clients": clients
             }, ojf)
 
 
